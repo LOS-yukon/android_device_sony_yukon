3f72aa165bf278725a6d510c7270199bd055a80b
Konstantinos Pliakas <kj77@users.noreply.github.com>
2017-01-15 17:37:31 +0200

    Update media files (#3)
    Use CAF files taken from https://source.codeaurora.org/quic/la/platform/vendor/qcom/msm8226/tree/media?h=LA.BF.1.1.3_rb1.13
    
    -Fixes 1080p recording on eagle
    
    -Introducing media_codecs_performance which may improve FPS as described here
    https://source.android.com/devices/media/oem.html#2_achievable_frame_rates_for_video_codecs

--- platform.mk
+++ platform.mk
@@ -25,6 +25,7 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     $(SONY_ROOT)/system/etc/audio_policy.conf:system/etc/audio_policy.conf \
     $(SONY_ROOT)/system/etc/media_codecs.xml:system/etc/media_codecs.xml \
+    $(SONY_ROOT)/system/etc/media_codecs_performance.xml:system/etc/media_codecs_performance.xml \
     $(SONY_ROOT)/system/etc/media_profiles.xml:system/etc/media_profiles.xml \
     $(SONY_ROOT)/system/vendor/etc/audio_effects.conf:system/vendor/etc/audio_effects.conf
 
