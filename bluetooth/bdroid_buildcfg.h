4d1abc4e8fd8219ab6531c4580b90420f49bdd74
kpliakas <kjpliakas@gmail.com>
2017-09-03 11:12:43 +0300

    Revert "Disable CLOCK_BOOTTIME_ALARM"
    This reverts commit 029cf592ea11dcca4e57205b1d1c636c4057c12a.

--- bluetooth/bdroid_buildcfg.h
+++ bluetooth/bdroid_buildcfg.h
@@ -19,7 +19,4 @@
 
 #define BTA_HOST_INTERLEAVE_SEARCH  TRUE
 
-/* Defined if the kernel does not have support for CLOCK_BOOTTIME_ALARM */
-#define KERNEL_MISSING_CLOCK_BOOTTIME_ALARM TRUE
-
 #endif
