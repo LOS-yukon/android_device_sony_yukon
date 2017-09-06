67502ab0c5340b3655c28b1d9ff8934377b9b7df
kpliakas <kjpliakas@gmail.com>
2017-09-06 23:39:34 +0300

    Move wifi conf files under /vendor

--- platform.mk
+++ platform.mk
@@ -45,8 +45,8 @@ PRODUCT_COPY_FILES += \
 
 # Qualcom WiFi
 PRODUCT_COPY_FILES += \
-    $(SONY_ROOT)/system/etc/wifi/wpa_supplicant_overlay.conf:system/etc/wifi/wpa_supplicant_overlay.conf \
-    $(SONY_ROOT)/system/etc/wifi/p2p_supplicant_overlay.conf:system/etc/wifi/p2p_supplicant_overlay.conf
+    $(SONY_ROOT)/system/vendor/etc/wifi/wpa_supplicant_overlay.conf:system/vendor/etc/wifi/wpa_supplicant_overlay.conf \
+    $(SONY_ROOT)/system/vendor/etc/wifi/p2p_supplicant_overlay.conf:system/vendor/etc/wifi/p2p_supplicant_overlay.conf
 
 # Qualcom BT
 PRODUCT_COPY_FILES += \
