028e4a325368bf13f565934a4216e7c391a5e0da
kpliakas <kjpliakas@gmail.com>
2017-09-06 18:23:58 +0300

    Remove useless hostapd stuff

--- platform.mk
+++ platform.mk
@@ -46,8 +46,7 @@ PRODUCT_COPY_FILES += \
 # Qualcom WiFi
 PRODUCT_COPY_FILES += \
     $(SONY_ROOT)/system/etc/wifi/wpa_supplicant_overlay.conf:system/etc/wifi/wpa_supplicant_overlay.conf \
-    $(SONY_ROOT)/system/etc/wifi/p2p_supplicant_overlay.conf:system/etc/wifi/p2p_supplicant_overlay.conf \
-    $(SONY_ROOT)/system/etc/hostapd/hostapd_default.conf:system/etc/hostapd/hostapd_default.conf
+    $(SONY_ROOT)/system/etc/wifi/p2p_supplicant_overlay.conf:system/etc/wifi/p2p_supplicant_overlay.conf
 
 # Qualcom BT
 PRODUCT_COPY_FILES += \
