fe8ff73030bd639b4116e2b546c8b906b5f4375e
kpliakas <kjpliakas@gmail.com>
2017-09-09 16:42:14 +0300

    Revert "Remove media_codecs_performance.xml entry"
    This reverts commit 7a621e9b2b520acf9ea71cef0bdda870c9f7fb20.

--- platform.mk
+++ platform.mk
@@ -34,6 +34,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(SONY_ROOT)/system/etc/audio_policy.conf:system/etc/audio_policy.conf \
     $(SONY_ROOT)/system/etc/media_codecs.xml:system/etc/media_codecs.xml \
+    $(SONY_ROOT)/system/etc/media_codecs_performance.xml:system/etc/media_codecs_performance.xml \
     $(SONY_ROOT)/system/etc/media_profiles.xml:system/etc/media_profiles.xml \
     $(SONY_ROOT)/system/vendor/etc/audio_effects.conf:system/vendor/etc/audio_effects.conf
 
