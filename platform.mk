9e024db3a3740fdd5b5ea1d6e17f283f0aef24af
kpliakas <kjpliakas@gmail.com>
2017-11-24 10:11:50 +0200

    Build more hidl stuff

--- platform.mk
+++ platform.mk
@@ -163,11 +163,13 @@ PRODUCT_PACKAGES += \
 
 # DRM
 PRODUCT_PACKAGES += \
-    android.hardware.drm@1.0-impl
+    android.hardware.drm@1.0-impl \
+    android.hardware.drm@1.0-service
 
 # Display HIDL interfaces
 PRODUCT_PACKAGES += \
     android.hardware.graphics.allocator@2.0-impl \
+    android.hardware.graphics.allocator@2.0-service \
     android.hardware.graphics.composer@2.1-impl \
     android.hardware.graphics.mapper@2.0-impl \
     android.hardware.memtrack@1.0-impl
